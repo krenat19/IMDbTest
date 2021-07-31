9f978bdc1bbf3a0f07d2376081dbe7d291c43f3d
Reni <keszte.renata@gmail.com>
2021-07-31 14:31:09 +0200

    initial test added

---
+++ src/main/java/SearchResults.java
@@ -0,0 +1,6 @@
+import org.openqa.selenium.By;
+
+public class SearchResults {
+
+    public static By HeaderTitle = By.className("findHeader");
+}
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,8 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -18,7 +20,7 @@ public class MainPageTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-       // options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
 
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -27,12 +29,16 @@ public class MainPageTest {
 
     @Test
     public void Search() {
+        String searchTitle = "Terminator";
         driver.get("https://www.imdb.com/");
-        driver.findElement(MainPage.SearchBox).sendKeys("Terminator");
+        driver.findElement(MainPage.SearchBox).sendKeys(searchTitle);
         driver.findElement(MainPage.SearchButton).click();
+
+        WebElement HeaderText = driver.findElement(SearchResults.HeaderTitle);
+        Assertions.assertEquals("Results for " + "\"" + searchTitle + "\"", HeaderText.getText());
     }
 
-   // @AfterEach
+    @AfterEach
     public void Close() {
         driver.quit();
     }
